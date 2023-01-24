71cbf584bf1e163dc7f81f700ec28ec012761af8
akaecliptic <michealfatoye@gmail.com>
2023-01-24 22:52:54 +0000

    Updated search bar UI

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,36 +1,25 @@
 import { FC } from "react";
 import { FiSearch } from "react-icons/fi";
-import { OnChangeListener, VoidConsumer } from "types/functions";
+import { OnChangeListener } from "types/functions";
 import "styles/components/SearchBar.scss";
 
 const SearchBar: FC<{}> = () => {
-
-    const click: VoidConsumer = ( ) => {
-        const textArea = document.getElementById('input-search') as HTMLInputElement;
-        textArea.disabled = false;
-        textArea.focus();
-        textArea.addEventListener('focusout', focusout);
-    };
-
-    const focusout: VoidConsumer = ( ) => {
-        const textArea = document.getElementById('input-search') as HTMLInputElement;
-        textArea.disabled = true;
-    };
 
     const textChange: OnChangeListener = ( event ) => {
         console.log(event.target.value);
     };
 
     return (
-        <div id='container-search' onClick={click}>
-            <FiSearch id='icon-search'/>
+        <div id='container-search'>
             <input 
                 type='text' 
                 id='input-search' 
                 name='input-search' 
                 placeholder='Search'
-                onChange={textChange}
-                disabled />
+                onChange={textChange}/>
+            <div id='icon-search'>
+                <FiSearch/>
+            </div>
         </div>
     );
 };
