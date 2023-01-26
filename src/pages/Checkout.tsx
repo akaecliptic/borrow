ece5ebaf1d4dcb02c2c5182b06ab7f920e8dff08
akaecliptic <michealfatoye@gmail.com>
2023-01-26 03:52:26 +0000

    Added functionality to checkout back button

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 import TextButton from "components/TextButton";
 import CheckoutForm from "components/CheckoutForm";
 import "styles/pages/Checkout.scss";
 
 const Checkout: FC<{}> = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <header id='header-checkout'>
-                <TextButton text='<- BACK' click={ () => { console.log(' Hello World ')}}/>
+                <TextButton text='<- BACK' click={ () => { navigate('/') }}/>
             </header>
             <main>
                 <CheckoutForm />
