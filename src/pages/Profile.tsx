afe9c5c174547a569547caaacd64ddc079b68069
akaecliptic <michealfatoye@gmail.com>
2023-02-13 02:47:58 +0000

    Added initial styles for profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,36 @@
 import { FC } from "react";
+import { OnClickListener } from "types/functions";
+import "styles/pages/Profile.scss";
 
 const Profile: FC<{}> = () => {
+
+    const click: OnClickListener = ( event ) => {
+        if ( event.target instanceof HTMLDivElement ) return;
+
+        //! WATCH: Can't place it, but this looks odd. Migtht come back
+        const children: HTMLElement[] = Array.from(event.currentTarget.children) as HTMLElement[];
+
+        children.forEach( child => {
+            if ( child === event.target ) {
+                if ( !child.classList.contains('active') ) child.classList.add('active');
+            } else {
+                child.classList.remove('active');
+            }
+        });
+    };
+
     return (
         <>
-            <div>Profile</div>
+            <div className='form-container'>
+                <div className='form-tabs' onClick={click}>
+                    <span className='active'>Account</span>
+                    <span>User</span>
+                    <span>Borrowed</span>
+                </div>
+                <div className='form'>
+                
+                </div>
+            </div>
         </>
     );
 };
