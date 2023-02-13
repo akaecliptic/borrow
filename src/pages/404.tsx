2037bae01c2e2d75fb0f7dfc366bcd4b97ee068a
akaecliptic <michealfatoye@gmail.com>
2023-02-13 22:00:04 +0000

    Updated 404 page, adding inline styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 
 const Error404: FC<{}> = () => {
     return (
-        <>
-            <h1>404 Error</h1>
-            <h3>Hello, this is an error</h3>
-        </>
+        <div style={{ margin: 'auto' }}>
+            <h1 style={{ textAlign: 'center' }}>404 ERROR</h1>
+            <h3>Hmm... The page you are looking for does not exist</h3>
+        </div>
     );
 };
 
